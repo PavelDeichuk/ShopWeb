93eb01e1203cf8ec4fd87fc18a18cac3c08b3d8e
PavelDeichuk <deychuk@inbox.ru>
2023-01-24 19:14:32 +0300

    Add UsersService

---
+++ src/main/java/com/pavel/shopweb/Service/UsersService.java
@@ -0,0 +1,25 @@
+package com.pavel.shopweb.Service;
+
+import com.pavel.shopweb.Dto.UsersDto;
+import com.pavel.shopweb.Entity.UsersEntity;
+import org.springframework.validation.BindingResult;
+
+import java.util.List;
+
+public interface UsersService {
+    List<UsersDto> GetAllUsers(int page, int size);
+
+    UsersDto GetUserById(Long user_id);
+
+    UsersDto CreateUser(UsersEntity usersEntity, BindingResult bindingResult);
+
+    UsersDto ActivateUser(String activationCode);
+
+    UsersDto ResetPassword(String email);
+
+    UsersDto ChangePassword(String passToken, UsersEntity usersEntity);
+
+    UsersDto EditUser(Long user_id, UsersEntity usersEntity);
+
+    UsersDto DeleteUser(Long user_id);
+}
