f771fba3fef3b004bb11b09c4d3bbd46f9deee87
PavelDeichuk <deychuk@inbox.ru>
2023-01-26 22:49:59 +0300

    Add ImageEntity

---
+++ src/main/java/com/pavel/shopweb/Entity/ImageEntity.java
@@ -0,0 +1,33 @@
+package com.pavel.shopweb.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.type.descriptor.jdbc.BinaryJdbcType;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "image")
+@DynamicUpdate
+@DynamicInsert
+public class ImageEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @Lob
+    @JdbcType(BinaryJdbcType.class)
+    private byte[] image;
+
+    private Long size;
+
+    @OneToOne(mappedBy = "imageEntity", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private UsersEntity usersEntity;
+}
