632d26c6c5540132c624b0bb00a62e00e45fdf6a
PavelDeichuk <deychuk@inbox.ru>
2023-01-26 23:59:00 +0300

    Update ImageController

--- src/main/java/com/pavel/shopweb/Controller/ImageController.java
+++ src/main/java/com/pavel/shopweb/Controller/ImageController.java
@@ -1,5 +1,6 @@
 package com.pavel.shopweb.Controller;
 
+import com.pavel.shopweb.Dto.ImageDto;
 import com.pavel.shopweb.Service.ImageService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ public class ImageController {
 
     private static final String IMAGE_NAME = "/{image_name}";
 
+    private static final String IMAGE_ID = "/{image_id}";
+
     public ImageController(ImageService imageService) {
         this.imageService = imageService;
     }
@@ -22,5 +25,10 @@ public class ImageController {
     @RequestMapping(value = IMAGE_NAME, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public byte[] GetImage(@PathVariable String image_name){
         return imageService.GetImageByName(image_name);
+    }
+
+    @RequestMapping(value = IMAGE_ID, method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ImageDto DeleteImage(@PathVariable Long image_id){
+        return imageService.DeleteImage(image_id);
     }
 }
