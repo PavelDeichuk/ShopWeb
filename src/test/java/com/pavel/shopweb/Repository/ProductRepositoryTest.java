9ad8a606d625376d4d7f1eab65cff92f6bb1609c
pavel <deychuk@inbox.ru>
2023-02-06 18:46:01 +0300

    Update ProductRepositoryTest

--- src/test/java/com/pavel/shopweb/Repository/ProductRepositoryTest.java
+++ src/test/java/com/pavel/shopweb/Repository/ProductRepositoryTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.ArrayList;
@@ -21,6 +25,20 @@ class ProductRepositoryTest {
 
     @MockBean
     private ProductRepository productRepository;
+
+    @Container
+    public static PostgreSQLContainer postgreSQLContainer =
+            new PostgreSQLContainer("postgres:13.3")
+                    .withUsername("root")
+                    .withPassword("root")
+                    .withDatabaseName("shop");
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+    }
 
 
     @Test
