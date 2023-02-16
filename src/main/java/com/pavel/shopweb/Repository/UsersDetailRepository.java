80b8baadc227c59ffe76312158b922d3b3facc5f
pavel <deychuk@inbox.ru>
2023-02-16 15:38:17 +0300

    Update UsersDetailRepository

--- src/main/java/com/pavel/shopweb/Repository/UsersDetailRepository.java
+++ src/main/java/com/pavel/shopweb/Repository/UsersDetailRepository.java
@@ -4,8 +4,18 @@ import com.pavel.shopweb.Entity.UsersDetailEntity;
 import com.pavel.shopweb.Entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UsersDetailRepository extends JpaRepository<UsersDetailEntity,Long> {
     @Query("SELECT u FROM UsersDetailEntity u WHERE u.usersEntity = ?1")
     UsersDetailEntity findByUsersEntity(UsersEntity usersEntity);
+
+    @Query(value = "SELECT * FROM users_detail " +
+            "WHERE (SELECT mailing FROM users WHERE mailing = true) " +
+            "AND extract(MONTH FROM birthday) = :m " +
+            "AND extract(DAY FROM birthday) = :d",
+            nativeQuery = true)
+    List<UsersDetailEntity> findByMatchMonthAndMatchDay(@Param("m") int month, @Param("d") int day);
 }
